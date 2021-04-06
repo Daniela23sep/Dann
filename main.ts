8b715c2e0c3a1560935d81a9608caeafa0117b2f
Daniela23sep <82041630+Daniela23sep@users.noreply.github.com>
2021-04-06 16:45:49 +0200

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,14 @@
-
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Happy)
+    basic.showString("G R A C I A S ")
+})
+input.onGesture(Gesture.Shake, function () {
+    music.playTone(349, music.beat(BeatFraction.Sixteenth))
+    basic.showString("G E L !")
+})
+input.onButtonPressed(Button.B, function () {
+    for (let index = 0; index < 4; index++) {
+        basic.showIcon(IconNames.Heart)
+        basic.showIcon(IconNames.SmallHeart)
+    }
+})
